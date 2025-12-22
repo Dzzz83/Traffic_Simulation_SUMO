a4496f84b6b11095af6fc30bfcce4f1f0e5e62b8
Dzzz83 <phattuan43@gmai.com>
2025-12-22 17:39:15 +0100

    create new file for 3D rendering

---
+++ src/app/MapDraw3D.java
@@ -0,0 +1,24 @@
+package app;
+
+import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.SubScene;
+import javafx.scene.SceneAntialiasing;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import wrapperSUMO.ControlPanel;
+import de.tudresden.sumo.objects.SumoPosition2D;
+
+public class MapDraw3D
+{
+    private Group root3D;
+    private SubScene subScene;
+    private PerspectiveCamera camera;
+
+}
